c8148b94106694221f65986ec1638519c136e452
lecorreromain2 <lecorreromain2@gmail.com>
2025-11-20 17:36:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig); 
 const database = firebase.database();
 
 // Variables globales
